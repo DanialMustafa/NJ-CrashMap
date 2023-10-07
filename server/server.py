441d67fb66b791e97684e6c4d2089dc026b4a23a
Thomas Wong <thomaskwwong7@gmail.com>
2023-10-07 19:25:27 -0400

    dashboard added

--- server/server.py
+++ server/server.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/members")
+@app.route("/stations")
 def members():
-    return {"members": ["Members1", "Members2", "Members3"],
-            "food": "pizza"}
+    return {"stations": ["Atlantic City", "Main-Bergen County", "Montclair-Bontoon", "Morris & Essex", "Northeast Corridor",
+                         "North Jersey Coast", "Pascack Valley", "Raritan Valley"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
